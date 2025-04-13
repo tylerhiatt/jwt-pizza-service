6b9f77f09a943b6b1ef02fd34bfeea41913e8bc2
tylerhiatt <tyler.hiatt@byu.edu>
2025-04-13 15:42:04 -0600

    New version

--- src/service.js
+++ src/service.js
@@ -23,7 +23,6 @@ app.use((req, res, next) => {
   if (origin) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
-  //res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
